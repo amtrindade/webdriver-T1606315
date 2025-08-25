2db989a17b1d0eb54960f7d73bd1c26b3af48e1a
Antonio Trindade <atrindade@umov.me>
2025-08-25 19:57:54 -0300

    Adiciona teste para soma com conversão de valores aleatórios

--- src/test/java/com/test/CalculatorTest.java
+++ src/test/java/com/test/CalculatorTest.java
@@ -2,6 +2,8 @@ package com.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,7 +25,6 @@ public class CalculatorTest {
 
 	@AfterEach
 	public void tearDown() throws Exception {
-		Thread.sleep(3000);
 		driver.quit();
 	}
 	
@@ -44,5 +45,37 @@ public class CalculatorTest {
 		Thread.sleep(3000);
 		
 		assertEquals("13", tfTotal.getAttribute("value"));		
+	}
+	
+	@Test
+	public void testSomaComConversao() throws InterruptedException {
+		
+		Random random = new Random();
+
+        // Define o valor máximo (100). O valor mínimo é 1.
+        int valorMaximo = 100;
+        int valorMinimo = 1;
+		
+		Integer valor1 = random.nextInt(valorMaximo) + valorMinimo;
+		Integer valor2 = random.nextInt(valorMaximo) + valorMinimo;
+		
+		WebElement tfNumber1 = driver.findElement(By.id("number1"));
+		WebElement tfNumber2 = driver.findElement(By.id("number2"));
+		WebElement tfTotal = driver.findElement(By.id("total"));
+		WebElement btnSomar = driver.findElement(By.id("somar"));
+		
+		//Operação matemática de soma
+		Integer valorTotal = valor1 + valor2;
+		
+		tfNumber1.sendKeys(valor1.toString());
+		tfNumber2.sendKeys(valor2.toString());
+		
+		btnSomar.click();
+		
+		//TODO corrigir espera pelo texto do elemento
+		Thread.sleep(3000);
+		
+		assertEquals(valorTotal.toString(), tfTotal.getAttribute("value"));			
+		
 	}
 }
