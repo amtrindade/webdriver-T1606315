f5be5a9122e8ded19f0f7b2e10308bb067f9807d
Antonio Trindade <atrindade@umov.me>
2025-08-26 20:06:22 -0300

    Adiciona classe RegularExpressionTest com teste de validação de CPF

---
+++ src/test/java/com/test/RegularExpressionTest.java
@@ -0,0 +1,50 @@
+package com.test;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegularExpressionTest {
+	
+	public WebDriver driver;
+
+	@BeforeEach
+	public void setUp() throws Exception {		
+		System.setProperty("webdriver.geckodriver.driver", "/Users/umov.me/Dev/drivers/geckodriver");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		driver.get("https://www.geradordecpf.org/");		
+	}
+
+	@AfterEach
+	public void tearDown() throws Exception {
+		Thread.sleep(3000);
+		driver.quit();
+	}
+	
+	@Test
+	public void testValidationCPFWithPoint() {
+		WebElement cbPontos = driver.findElement(By.id("cbPontos"));
+		cbPontos.click();
+		
+		WebElement btnGerar = driver.findElement(By.id("btn-gerar-cpf"));
+		btnGerar.click();
+		
+		WebElement tfCpf = driver.findElement(By.id("numero"));
+		String cpfGerado = tfCpf.getAttribute("value");
+		
+		System.out.println(cpfGerado);
+		
+		assertTrue(cpfGerado.matches("^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$"));
+	}
+
+}
